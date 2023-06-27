91d658c89fde101e00aaae1e9f502fd39032fed8
goringich <actingsv@gmail.com>
2023-06-27 18:54:17 +0300

    Описание ваших изменений

--- assets/js.js
+++ assets/js.js
@@ -5,5 +5,5 @@ let order = document.getElementById("order")
 buy.addEventListener("click", () => {
   document.getElementsByTagName("h1").style.display="block"
   alert("Are you sure?")
-  document.getElementById("input_name").value = tg.initDataUnsafe.first_name+" " + tg.initData.last_name
+  document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name+" " + tg.initData.user.last_name
 })
