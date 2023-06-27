622e256596594a8d01997c2865039b448800981c
goringich <actingsv@gmail.com>
2023-06-27 19:08:38 +0300

    he

--- assets/js.js
+++ assets/js.js
@@ -1,9 +1,11 @@
 let tg=window.Telegram.WebApp
 let buy = document.getElementById("buy")
 let order = document.getElementById("order")
+tg.expend() // open the page to full screen
 
 buy.addEventListener("click", () => {
   document.getElementsByTagName("h1").style.display="block"
   alert("Are you sure?")
+  // name input
   document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name+" " + tg.initData.user.last_name
 })
