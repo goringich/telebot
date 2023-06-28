c26567517c3c5e0e37a4745268100f09a3449931
goringich <actingsv@gmail.com>
2023-06-28 21:23:09 +0300

    ╨Ю╨┐╨╕╤Б╨░╨╜╨╕╨╡ ╨▓╨░╤И╨╕╤Е ╨╕╨╖╨╝╨╡╨╜╨╡╨╜╨╕╨╣

--- assets/js.js
+++ assets/js.js
@@ -5,10 +5,10 @@ let error= document.getElementById("input_error")
 error.style.display="none"
 tg.expand() // open the page to full screen
 // name input
-document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name
-document.getElementById("input_surname").value = tg.initDataUnsafe.user.last_name
 
 buy.addEventListener("click", () => {
+  document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name
+  document.getElementById("input_surname").value = tg.initDataUnsafe.user.last_name
   document.getElementsByTagName("h1")[0].style.display="block"
 
   a=["name","surname",'text','mail',"phone"]
