690f44aaad678c93ace332bb58d3b0320ce510cb
goringich <actingsv@gmail.com>
2023-06-28 14:53:16 +0300

    ╨Ю╨┐╨╕╤Б╨░╨╜╨╕╨╡ ╨▓╨░╤И╨╕╤Е ╨╕╨╖╨╝╨╡╨╜╨╡╨╜╨╕╨╣

--- assets/js.js
+++ assets/js.js
@@ -2,11 +2,13 @@ let tg=window.Telegram.WebApp
 let buy = document.getElementById("buy")
 let order = document.getElementById("order")
 tg.expend() // open the page to full screen
+document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name
 
 buy.addEventListener("click", () => {
   document.getElementsByTagName("h1").style.display="block"
   alert("Are you sure?")
   // name input
   console.log("hello world!")
-  document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name+" " + tg.initDataUnsafe.user.last_name
+  console.log(tg.initDataUnsafe.user)
+  
 })
