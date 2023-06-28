2c4a408b3b335eec09ab7f19269c6dac649425a2
goringich <actingsv@gmail.com>
2023-06-28 16:07:26 +0300

    ╨Ю╨┐╨╕╤Б╨░╨╜╨╕╨╡ ╨▓╨░╤И╨╕╤Е ╨╕╨╖╨╝╨╡╨╜╨╡╨╜╨╕╨╣

--- assets/js.js
+++ assets/js.js
@@ -1,7 +1,7 @@
 let tg = window.Telegram.WebApp
 let buy = document.getElementById("buy")
 let order = document.getElementById("order")
-let error= document.getElementsById("input_error")
+let error= document.getElementById("input_error")
 error.style.display="none"
 tg.expand() // open the page to full screen
 // name input
@@ -9,11 +9,11 @@ document.getElementById("input_name").value = tg.initDataUnsafe.user.first_name
 document.getElementById("input_surname").value = tg.initDataUnsafe.user.last_name
 
 buy.addEventListener("click", () => {
-  document.getElementsByTagName("h1").style.display="block"
+  document.getElementsByTagName("h1")[0].style.display="block"
 
   a=["name","surname",'text']
   for (i in a){
-    if (document.getElementsById("input"+i).value.length<=2){
+    if (document.getElementById("input"+i).value.length<=2){
       error.innerText = "Error in" + i
       error.display="block"
     }
